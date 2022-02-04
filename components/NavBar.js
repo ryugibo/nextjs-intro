2a8b6204c9f960b844dc50f774b0fe40aaa1bcfc
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-02-04 17:44:18 +0900

    :construction: #1.4 | CSS Modules
    - css module 을 사용하면 모듈간에 중복되는 이름을 피할 수 있다.
    - 다수의 이름은 문자열로 변환해서 지정해주어야 한다.

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,16 +1,25 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
   console.log(router);
 
-  return <nav>
-    <Link href="/">
-      <a style={{ color: router.pathname === "/" ? "red" : "blue" }}>Home</a>
-    </Link>
-    <Link href="/about">
-      <a style={{ color: router.pathname === "/about" ? "red" : "blue" }}>About</a>
-    </Link>
-  </nav>
+  return (
+    <nav>
+      <Link href="/">
+        <a className={`
+          ${ styles.link }
+          ${ router.pathname === "/" ? styles.active : "" }
+        `}>Home</a>
+      </Link>
+      <Link href="/about">
+        <a className={[
+          styles.link,
+          router.pathname === "/about" ? styles.active : ""
+        ].join(" ")}>About</a>
+      </Link>
+    </nav>
+  )
 }
