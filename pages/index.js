e9cead0131a00abdecc1f52c86baeb95b30c59e8
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-02-04 21:48:55 +0900

    :construction: #2.1 | Fetching Data

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
 import Seo from "../components/Seo";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [movies, setMovies] = useState();
+
+  useEffect(() => {
+    (async() => {
+      const { results } = await (await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)).json();
+      setMovies(results);
+    })();
+  }, []);
+
   return (
-    <div>
+    <div> 
       <Seo title="Home" />
-      <h1>Hello</h1>
+      { !movies && <h4>Loading..</h4>}
+      { movies?.map(movie => (
+        <div key={ movie.id }>
+          <h4>{ movie.original_title }</h4>
+        </div>
+      ))}
     </div>
   )
 }
