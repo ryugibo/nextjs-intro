d323ecdbafb7a3fbcede82ea266f44facb45d22c
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-02-04 21:16:13 +0900

    :construction: #2.0 | Patterns
    - 반복되는 내용을 컴포넌트로 나누고 Props에 정보를 주기

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,12 @@
-import NavBar from "../components/NavBar";
-import "../styles/globals.css"
+import Layout from "../components/Layout";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   // @NOTE: default
   // return  <Component {...pageProps} />;
   return <>
-    <NavBar />
-    <Component { ...pageProps } />
-      <style jsx global>
-      {`
-        a {
-          color: white;
-        }
-      `}
-      </style>
+    <Layout>
+      <Component { ...pageProps } />
+    </Layout>
   </>;
 }
