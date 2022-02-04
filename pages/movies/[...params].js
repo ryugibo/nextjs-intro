da0aa1b335579bee2cee0fb3a55895d5d38f3d21
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-02-04 23:23:25 +0900

    :construction: #2.7 | Catch All

---
+++ pages/movies/[...params].js
@@ -0,0 +1,19 @@
+import { useRouter } from "next/router";
+import Seo from "../../components/Seo";
+
+export default function Detail({ params }) {
+  console.log(params);
+  const router = useRouter();
+  const [title, id] = params || [];
+  console.log(router);
+  return (
+    <div>
+      <Seo title={ title } />
+      <h4>{ title }</h4>
+    </div>
+  );
+}
+
+export function getServerSideProps({ params: { params } }) {
+  return { props: { params } };
+}
